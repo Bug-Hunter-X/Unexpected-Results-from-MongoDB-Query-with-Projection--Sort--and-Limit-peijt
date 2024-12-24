35446809490c07fbedd22a0651a27777fa74e5d0
BugHunter-X <askzenai@gmail.com>
2024-12-24 18:46:08 +0100

    Add bug.js file

---
+++ bug.js
@@ -0,0 +1,16 @@
+```javascript
+const query = { field: 'value' };
+const options = {
+  projection: { _id: 0, field1: 1, field2: 1 },
+  sort: { field: -1 },
+  limit: 10,
+};
+
+db.collection('myCollection').find(query, options).toArray((err, result) => {
+  if (err) {
+    console.error('Error fetching documents:', err);
+  } else {
+    console.log('Fetched documents:', result);
+  }
+});
+```
